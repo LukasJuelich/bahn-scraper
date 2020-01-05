76e7bb7b9f93ef2911c3d5c5c031054e24c2fc0c
Lukas Jülich <lukas.juelich@rwth-aachen.de>
2020-01-05 01:14:31 +0100

    refactor

--- bahn_scraper.js
+++ bahn_scraper.js
@@ -3,7 +3,47 @@ const cheerio = require("cheerio");
 const moment = require("moment");
 const MongoClient = require('mongodb').MongoClient;
 
-var dbURL = "mongodb://localhost:27017/mydb";
+const dbURL = "mongodb://localhost:27017/mydb";
+var startStation = "horrem";
+var endStation ="Aachen"
+
+moment.locale("de");
+var url = "https://reiseauskunft.bahn.de/bin/query.exe/dn?&start=1"
+            + "&S=" + startStation
+            + "&Z=" + endStation
+            + "&date=" + moment().format("D.M.Y")
+            + "&time=" + moment().format("LT");
+console.log(url);
+
+const getHtmlWithPuppeteer = (async (url) =>{
+    var html;
+    
+    try{
+        const browser = await puppeteer.launch({headless: false});   //for Raspberry Pi {executablePath: '/usr/bin/chromium-browser'}
+        const page = await browser.newPage();
+        await page.goto(url);
+        html = await page.content();
+        await browser.close();
+    }
+    catch(err) {
+        console.log("Error in puppeteer: " + err);
+    }
+
+    return html;
+})();
+
+const getDepartureAndDelay = (html) => {
+    var $ = cheerio.load(html);
+
+    var time = $(".time").eq(1)[0];
+    var plannedDeparture = time.firstChild.nodeValue;
+    var delay = time.firstChild.nextSibling.lastChild.nodeValue;
+
+    console.log(plannedDeparture + "\n" + delay);
+
+    return [plannedDeparture, delay];
+}
+
 
 MongoClient.connect(dbURL, {useUnifiedTopology: true}, function(err, db) {
     if(err) throw err;
@@ -13,42 +53,20 @@ MongoClient.connect(dbURL, {useUnifiedTopology: true}, function(err, db) {
         if(err) throw err;
         console.log("DB connection established!");
 
-        moment.locale("de");
-        var url = "https://reiseauskunft.bahn.de/bin/query.exe/dn?revia=yes&existOptimizePrice=1&country=DEU&dbkanal_007=L01_S01_D001_KIN0001_qf-bahn-svb-kl2_lz03&start=1&protocol=https%3A&REQ0JourneyStopsS0A=1&S=Horrem+Bahnhof%2C+Kerpen+(Rheinland)&REQ0JourneyStopsSID=A%3D1%40O%3DHorrem+Bahnhof%2C+Kerpen+(Rheinland)%40X%3D6713621%40Y%3D50915801%40U%3D80%40L%3D000443575%40B%3D1%40p%3D1556573609%40&Z=Aachen+Hbf&REQ0JourneyStopsZID=&date=Mo%2C"
-        + moment().format("D.M.Y") + "&time=" + moment().format("LT") + "&timesel=depart&returnDate=&returnTime=&returnTimesel=depart&optimize=0&auskunft_travelers_number=1&tariffTravellerType.1=E&tariffTravellerReductionClass.1=0&tariffClass=2&rtMode=DB-HYBRID&externRequest=yes&HWAI=JS!js%3Dyes!ajax%3Dyes!";
-        console.log(url);
+        let html = getHtmlWithPuppeteer(url);
+        let array = getDepartureAndDelay(html);
 
-        (async () =>{
-            try{
-                const browser = await puppeteer.launch();   //for Raspberry Pi {executablePath: '/usr/bin/chromium-browser'}
-                const page = await browser.newPage();
-                await page.goto(url);
-                var html = await page.content();
-                await browser.close();
-            }
-            catch(err) {
-                console.log("Error in puppeteer: " + err);
-                db.close();
-                console.log("DB connection closed!");
-            }
+        let plannedDeparture = array[0];
+        let delay = array[1];
 
-            var $ = cheerio.load(html);
-
-            var time = $(".time").eq(1)[0];
-            var plannedDeparture = time.firstChild.nodeValue;
-            var delay = time.firstChild.nextSibling.lastChild.nodeValue;
-
-            console.log(plannedDeparture + "\n" + delay);
-
-            var dbEntry = { abfrage: moment().format("LLLL"),
-                            abfahrt: plannedDeparture,
-                            verspaetung: delay};
-            dbo.collection("times").insertOne(dbEntry, function(err, res) {
-                if(err) throw err;
-                console.log("1 document inserted!");
-                db.close();
-                console.log("DB connection closed!");
-            });
-        })();
+        var dbEntry = { abfrage: moment().format("LLLL"),
+                        abfahrt: plannedDeparture,
+                        verspaetung: delay};
+        dbo.collection("times").insertOne(dbEntry, function(err, res) {
+            if(err) throw err;
+            console.log("1 document inserted!");
+            db.close();
+            console.log("DB connection closed!");
+        });
     });
 });
